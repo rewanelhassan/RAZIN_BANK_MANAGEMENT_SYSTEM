8eae75a8446a2f3f991df8a65724fce4a2ee9d17
Zeinab304 <zozty1589@gmail.com>
2026-03-11 03:53:56 +0200

    Include additional headers for file and data management
    Added new header files for file management and data handling.

--- RAZIN_BANK_SYSTEM/RAZIN_BANK_SYSTEM.cpp
+++ RAZIN_BANK_SYSTEM/RAZIN_BANK_SYSTEM.cpp
@@ -1,12 +1,22 @@
  
 #include <iostream>
 #include<vector>
-#include"string";
+#include<fstream>
+#include <sstream>
+#include<string>
+
 #include"Validation.h"
+#include "Parser.h"
+
+#include "FilesHelper.h"
+#include "DataSourceInterface.h"
+#include "FileManager.h"
+
 #include"Person.h"
 #include"Client.h"
 #include"Employee.h"
 #include"Admin.h"
+
 using namespace std;    
 
 int main()
